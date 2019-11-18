7afa17d1004ad5698364fcc62d6fbefe482a36c7
Jeffrey Brown <brownjs03@gmail.com>
2019-11-18 01:48:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,15 @@ class Transfer
   end
 
 #def execute_transaction
- # if @sender.balance > amount && status == "pending"
-  #@sender.balance -= amount
-  #@receiver.balance += amount
-  #@status = "complete"
-  #else 
-   # @status = "rejected"
-    #return "Transaction rejected. Please check your account balance."
-  #end
-#end
+  if @sender.balance > amount && status == "pending"
+  @sender.balance -= amount
+  @receiver.balance += amount
+  @status = "complete"
+  else 
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+end
 
   
 end
